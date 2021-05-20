79171735026a9336dfb02c96382874904c9dbb45
zhangyuyf <zy123456>
2021-05-20 10:38:33 +0800

    前端模块化

--- demos/前端工程化/前端模块化/6. Rollup/src/head.js
+++
@@ -1,7 +0,0 @@
-// 导出一个箭头函数
-export default () => {
-  const element = document.createElement('h2')
-  element.textContent = 'Hello Rollup'
-  element.addEventListener('click', () => alert('Hello Rollup'))
-  return element
-}
--- demos/前端工程化/前端模块化/7. Parcel/src/head.js
+++
@@ -1,7 +0,0 @@
-// 导出一个箭头函数
-export default () => {
-  const element = document.createElement('h2')
-  element.textContent = 'Hello Parcel'
-  element.addEventListener('click', () => alert('Hello Parcel'))
-  return element
-}
