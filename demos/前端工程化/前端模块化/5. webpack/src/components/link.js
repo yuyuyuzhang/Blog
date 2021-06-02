60261c5ad2ca9ba6cbf9a5668490b7044a6a870b
zhangyuyf <zy123456>
2021-06-02 14:27:17 +0800

    webpack

--- demos/前端工程化/前端模块化/5. webpack/src/components/link.js
+++
@@ -1,5 +0,0 @@
-const link = document.createElement('a')
-link.href = 'https://www.baidu.com/'
-link.innerHTML = '百度一下'
-
-export default link // 默认导出
--- demos/前端工程化/前端模块化/5. webpack/src/components/pad.js
+++
@@ -1,5 +0,0 @@
-// 为 Number 的原型添加一个扩展方法
-Number.prototype.pad = function(size) {
-  const leadingZeros = Array(size + 1).join(0)
-  return leadingZeros + this
-}
